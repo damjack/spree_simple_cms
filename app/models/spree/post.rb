574315944f8d3d435178634dbea548dbe6e02e60
Riccardo Degan <riccardo.degan@diginess.it>
2012-07-02 15:16:31 +0200

    Add attr_accessible to post and static page model

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -2,7 +2,7 @@ module Spree
   class Post < Blog
     #acts_as_commentable
     belongs_to :post_category
-    
+    attr_accessible :name, :content, :published_at, :active, :tag_list
     scope :published, lambda { where("published_at <= '#{Time.now}' AND active = 1") }
     scope :valid, lambda { where("from <= #{Time.now} AND to >= #{Time.now}") }
     
--- app/models/spree/static_page.rb
+++ app/models/spree/static_page.rb
@@ -2,6 +2,7 @@ module Spree
   class StaticPage < Page
     belongs_to :product
     
+    attr_accessible :name, :slug, :tag_title, :meta_description, :meta_keywords, :content, :published_at, :image_width, :image_height
     scope :published, lambda { where("published_at <= ? AND published_at IS NOT NULL", Time.now) }
     
     def initialize(*args)
