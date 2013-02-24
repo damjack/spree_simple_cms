34bbaa4af4e69f7adc5deba2dfe6b79a63b30c74
Damiano <damiano.giacomello@diginess.it>
2013-02-24 11:10:12 +0100

    tried to fix contact mailer

--- app/mailers/spree/contact_mailer.rb
+++ app/mailers/spree/contact_mailer.rb
@@ -1,15 +1,13 @@
 module Spree
   class ContactMailer < ActionMailer::Base
     helper 'spree/base'
-    
-    default :from => Spree::Config[:store_default_email]
 
     def contacts_form(name, email, text)
       @name = name
       @email = email
       @messaggio = text
-      mail(:to => "#{email} - #{name}",
-      :subject => "[Spree Ecommerce] - Contatto dal sito web")
+      mail(:to => "#{email} <#{name}>",
+      :subject => "#{Spree::Config[:site_name]} - Contatto dal sito web")
     end
   end
 end
