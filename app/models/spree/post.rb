7e8b48bfd2837948a9320d8de97240f4469c93ca
Damiano <damiano.giacomello@diginess.it>
2013-03-13 13:50:23 +0100

    fixed error for post model

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,7 +1,7 @@
 module Spree
   class Post < Blog
-    has_and_belongs_to_many :products, :join_table => 'spree_posts_products', :dependent => :destroy
-    has_and_belongs_to_many :taxons, :join_table => 'spree_posts_taxons', :dependent => :destroy
+    has_and_belongs_to_many :products, :join_table => 'spree_posts_products'
+    has_and_belongs_to_many :taxons, :join_table => 'spree_posts_taxons'
     acts_as_commentable
     
     has_and_belongs_to_many :taxon_posts, :join_table => "spree_posts_taxon_posts", :class_name => "Spree::TaxonPost"
