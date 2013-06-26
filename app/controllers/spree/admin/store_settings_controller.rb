e8912eeb018ca827ea6ccf33da0b89bb7c9cf99a
Damiano <damiano.giacomello@diginess.it>
2013-06-26 09:27:04 +0200

    fixed configuration store and added new social uri. update version

--- app/controllers/spree/admin/store_settings_controller.rb
+++ app/controllers/spree/admin/store_settings_controller.rb
@@ -3,15 +3,17 @@ module Spree
     class StoreSettingsController < BaseController
       def show
         @preferences = ['store_name', 'store_default_email', 'store_address',
-                        'store_phone_number', 'store_fax_number', 'store_twitter_id',
-                        'store_facebook_id', 'store_pinterest_id', 'store_youtube_id', 'store_skype', 'store_location',
+                        'store_phone_number', 'store_fax_number', 'store_twitter_uri',
+                        'store_facebook_uri', 'store_pinterest_uri', 'store_youtube_uri',
+                        'store_google_plus_uri', 'store_linkedin_uri', 'store_skype', 'store_location',
                         'store_description', 'store_welcome_title', 'store_welcome_description']
       end
 
       def edit
         @preferences = [:store_name, :store_default_email, :store_address,
-                        :store_phone_number, :store_fax_number, :store_twitter_id,
-                        :store_facebook_id, :store_pinterest_id, :store_youtube_id, :store_skype, :store_location,
+                        :store_phone_number, :store_fax_number, :store_twitter_uri,
+                        :store_facebook_uri, :store_pinterest_uri, :store_youtube_uri,
+                        :store_google_plus_uri, :store_linkedin_uri, :store_skype, :store_location,
                         :store_description, :store_welcome_title, :store_welcome_description]
       end
 
--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -4,10 +4,14 @@ Spree::AppConfiguration.class_eval do
   preference :store_address, :string, :default => 'Address, 123 - 123456 City (IT)'
   preference :store_phone_number, :string, :default => '012 - 3456789'
   preference :store_fax_number, :string, :default => '012 - 3456789'
-  preference :store_twitter_id, :string, :default => 'twitter_id'
-  preference :store_facebook_id, :string, :default => 'facebook_id'
-  preference :store_pinterest_id, :string, :default => 'pinterest_id'
-  preference :store_youtube_id, :string, :default => 'youtube_id'
+  
+  preference :store_twitter_uri, :string, :default => 'twitter_uri'
+  preference :store_facebook_uri, :string, :default => 'facebook_uri'
+  preference :store_pinterest_uri, :string, :default => 'pinterest_uri'
+  preference :store_youtube_uri, :string, :default => 'youtube_uri'
+  preference :store_google_plus_uri, :string, :default => 'google_plus_uri'
+  preference :store_linkedin_uri, :string, :default => 'linkedin_uri'
+  
   preference :store_skype, :string, :default => 'ecommerce_skype'
   preference :store_location, :string, :default => 'google_maps_location'
   preference :store_description, :text, :default => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, sem id vulputate auctor, dui lectus lacinia ipsum, placerat euismod felis tellus a nulla. Nunc fermentum tincidunt dui, ac iaculis quam elementum et. Mauris sit amet vehicula tortor. Maecenas euismod fringilla venenatis. In accumsan ultricies odio at venenatis. Duis aliquam euismod libero, nec eleifend massa fermentum iaculis. Donec imperdiet egestas ligula pretium aliquam.'
